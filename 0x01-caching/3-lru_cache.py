152bfc1e71a65dea9082029f7539b41df1ed1922
Eric Munyi <irerieric@gmail.com>
2023-07-26 10:59:00 +0000

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -4,32 +4,56 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRU cache"""
+    """
+    LRUCache class that inherits from BaseCaching and implements the LRU caching system.
+    """
 
     def __init__(self):
-        """Initialiaze the LRU cache"""
+        """
+        Initialize the LRUCache class.
+        """
         super().__init__()
-        self.queue = []
+        # Initialize a list to keep track of the order in which items are accessed
+        self.usedKeys = []
 
     def put(self, key, item):
-        """adds an item in the cache"""
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.queue.remove(key)
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                """remove least used item from queue and cache"""
-                lru_item = self.queue.pop(0)
-                self.cache_data.pop(lru_item)
-                print("DISCARD:", lru_item)
+        """
+        Add an item to the LRUCache.
 
-            self.queue.append(key)
+        Args:
+            key (str): The key of the item to be added.
+            item (Any): The value of the item to be added.
+        """
+        if key is not None and item is not None:
+            # Add the item to the cache
             self.cache_data[key] = item
 
+            if key not in self.usedKeys:
+                # If the key is not already in usedKeys, append it to the list
+                self.usedKeys.append(key)
+            else:
+                # If the key is already in usedKeys, move it to the end (most recently used)
+                self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+
+            if len(self.usedKeys) > self.MAX_ITEMS:
+                # If the number of items in usedKeys exceeds the maximum capacity, evict the least recently used item
+                lru_key = self.usedKeys.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
+
     def get(self, key):
-        """get an item by key"""
+        """
+        Get an item from the LRUCache.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            The value of the item if it exists in the cache, otherwise None.
+        """
         if key is not None and key in self.cache_data:
-            """move accssed item to end of queue"""
-            self.queue.remove(key)
-            self.queue.append(key)
+            # If the key is valid and exists in the cache, move it to the end (most recently used)
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
             return self.cache_data[key]
         return None
+
