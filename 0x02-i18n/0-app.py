360fb8376fec1b245a75aa7368906d6ad340b514
Eric Munyi <irerieric@gmail.com>
2023-09-27 07:37:47 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,11 +7,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """route"""
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello() -> str:
+    """
+    return string
+    """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
